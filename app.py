ddb58381bceaccd9414889426a487fc895c7aa46
Devin Matte <devinmatte@gmail.com>
2017-10-27 21:31:42 -0400

    Trying to use the database connection

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from quotefaultAPI import app
 
 if __name__ == '__main__':
-    # app.run(host=app.config['IP'], port=app.config['PORT'], debug=True)
-    app.run(debug=True)
+    app.run(host=app.config['IP'], port=app.config['PORT'], debug=True)
+    # app.run(debug=True)
 
 application = app
--- quotefaultAPI/__init__.py
+++ quotefaultAPI/__init__.py
@@ -1,13 +1,46 @@
-from flask import Flask, jsonify
-
 import os
+import subprocess
+from datetime import datetime
+
+import requests
+from flask import Flask, render_template, request, flash, session, make_response, jsonify
+from flask_pyoidc.flask_pyoidc import OIDCAuthentication
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# if os.path.exists(os.path.join(os.getcwd(), "config.py")):
-#     app.config.from_pyfile(os.path.join(os.getcwd(), "config.py"))
-# else:
-#     app.config.from_pyfile(os.path.join(os.getcwd(), "config.env.py"))
+if os.path.exists(os.path.join(os.getcwd(), "config.py")):
+    app.config.from_pyfile(os.path.join(os.getcwd(), "config.py"))
+else:
+    app.config.from_pyfile(os.path.join(os.getcwd(), "config.env.py"))
+db = SQLAlchemy(app)
+
+# Disable SSL certificate verification warning
+requests.packages.urllib3.disable_warnings()
+
+# Disable SQLAlchemy modification tracking
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+auth = OIDCAuthentication(app,
+                          issuer=app.config['OIDC_ISSUER'],
+                          client_registration_info=app.config['OIDC_CLIENT_CONFIG'])
+
+app.secret_key = 'submission'  # allows message flashing, var not actually used
+
+
+# create the quote table with all relevant columns
+class Quote(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    submitter = db.Column(db.String(80))
+    quote = db.Column(db.String(200), unique=True)
+    speaker = db.Column(db.String(50))
+    quoteTime = db.Column(db.DateTime)
+
+    # initialize a row for the Quote table
+    def __init__(self, submitter, quote, speaker):
+        self.quoteTime = datetime.now()
+        self.submitter = submitter
+        self.quote = quote
+        self.speaker = speaker
 
 
 @app.route('/')
